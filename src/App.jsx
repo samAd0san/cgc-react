5d04dfc058db50f41da1ec75fedd18cb463adde1
samAd0san <sohaibsamad.hala@gmail.com>
2024-04-24 14:04:56 +0530

    Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import Skills from './Skills';
 import ProductList from './ProductList';
 import Counter from './Counter';
 
+import Header from './Header';
+import Footer from './Footer';
+
 function App() {
     return (
-        <div>
-            <Counter/>
-            <ProductList/>
-            {/* <Skills/> */}
-            {/* <Name value="sam" age={20}/>  */}
-            {/* <h1>CGC React</h1> */}
-            {/* <img width={200} height={200} src="https://images.pexels.com/photos/20628911/pexels-photo-20628911/free-photo-of-a-narrow-alley-with-buildings-and-a-street-sign.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Not Found" /> */}
-        </div>);
-};
+        // Styling footer to the bottom
+        <div className='flex flex-col h-screen justify-between'>
+            <Header />
+            <Footer />
+        </div>
+    );
+}
 
-
+// function App() {
+//     return (
+//         <div>
+//             <Counter/>
+//             <ProductList/>
+//             {/* <Skills/> */}
+//             {/* <Name value="sam" age={20}/>  */}
+//             {/* <h1>CGC React</h1> */}
+//             {/* <img width={200} height={200} src="https://images.pexels.com/photos/20628911/pexels-photo-20628911/free-photo-of-a-narrow-alley-with-buildings-and-a-street-sign.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Not Found" /> */}
+//         </div>);
+// };
 
 export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer() {
+    return(<footer className="w-full bg-gray-200 text-right p-2 h-10">
+        &copy;2024@cgc
+    </footer>)
+}
+export default Footer;

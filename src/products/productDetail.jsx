7e1d21c0eba64fd88869721405b3d5db1ce2ef21
samAd0san <sohaibsamad.hala@gmail.com>
2024-05-04 19:35:56 +0530

    Product Details Component

--- src/products/productDetail.jsx
+++ src/products/productDetail.jsx
@@ -1,7 +1,35 @@
-function ProductDetail() {
-    return (<div className="m-4 mt-15 p-2 text-center">
-        <h1 className="text-4xl font-semibold">Product Details</h1>
-    </div>);
+import React from "react";
+import ShouldRender from "../util/ShouldRender";
+import Error from "../util/Error";
+import ProductItem from "./ProductItem";
+import axios from "axios";
+
+class ProductDetail extends React.Component{
+
+    state = { product : null, hasError : false}
+
+    componentDidMount() {
+
+        const id = '66320a25d241a76d97198c09'; // To make it dynamic we will have to do function migration
+        const url = `http://localhost:3000/products/${id}`;
+        axios.get(url)
+            .then(res => this.setState({ product : res.data}))
+            .catch(() => this.setState({ hasError : true}))
+    }
+
+    render() {
+        return <div>
+            <ShouldRender when={this.state.hasError}>
+                <Error />
+            </ShouldRender>
+
+            <div className="w-h-screen flex items-center justify-center"> 
+            <ShouldRender when={this.state.product}>
+                <ProductItem product={this.state.product} />
+            </ShouldRender>
+            </div>
+        </div>
+    }
 }
 
 export default ProductDetail;

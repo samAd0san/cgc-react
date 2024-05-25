96ee5de56921cfbb9ce5cffca6fe9e289fbad4d7
samAd0san <sohaibsamad.hala@gmail.com>
2024-05-25 09:34:32 +0530

    Delete product by id

--- src/products/productDetail.jsx
+++ src/products/productDetail.jsx
@@ -3,7 +3,7 @@ import ShouldRender from "../util/ShouldRender";
 import Error from "../util/Error";
 import ProductItem from "./ProductItem";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../util/axios";
 
 function ProductDetail() {
@@ -12,22 +12,40 @@ function ProductDetail() {
     const [hasError, setError] = useState(false);
     const params = useParams();
 
+    const navigate = useNavigate();
+
+    const customRefresh = (id) => { // navigating to the /products page on deleting a product from productdetail
+        console.log('Deleted id is', id);
+        fetchData();
+        navigate('/products');
+    }
+
+    const fetchData = () => {
+        try {
+            const id = params.id;
+            const path = `/products/${id}`;
+            axiosInstance().get(path)
+                .then(res => setProduct(res.data))
+                .catch(() => setError(true))
+        } catch (err) {
+            setError(true);
+        }
+    }
+
     useEffect(() => {
-        const id = params.id;
-        const path = `/products/${id}`;
-        axiosInstance().get(path)
-            .then(res => setProduct(res.data))
-            .catch(() => setError(true))
+        fetchData();
     });
 
     return <div>
+        {/* Error */}
         <ShouldRender when={hasError}>
             <Error />
         </ShouldRender>
 
+        {/* Display the product */}
         <div className="w-h-screen flex items-center justify-center">
             <ShouldRender when={product}>
-                <ProductItem product={product} />
+                <ProductItem product={product} onItemDelete={customRefresh} />
             </ShouldRender>
         </div>
     </div>
